6c5b4fd29177b5d60a84308067533c9d83d2df64
Thiago Silva Ferreira <thiagosfsjc@gmail.com>
2016-11-17 21:45:19 -0200

    Configuracao para compilacao para producao do app

--- admin/webpack.config.js
+++ admin/webpack.config.js
@@ -30,6 +30,11 @@ module.exports = {
     }]
   },
   plugins: PROD ? [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
     new webpack.optimize.UglifyJsPlugin({
       compress: { warnings: false }
     })
