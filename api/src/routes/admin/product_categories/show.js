ecf3e47deb0540018ebd9a96995646e9549c4331
Thiago Silva Ferreira <thiagosfsjc@gmail.com>
2017-01-10 08:35:14 -0200

    Correcao da visualizacao de categorias no admin

--- api/src/routes/admin/product_categories/show.js
+++ api/src/routes/admin/product_categories/show.js
@@ -2,7 +2,7 @@ import { ProductCategory } from '../../../models'
 import { productCategoryView } from '../../../helpers'
 
 export default (req, res, next) => {
-  return ProductCategory.findOne({ _id: req.params.product_category_id }).then((product_category) => {
+  return ProductCategory.findById(req.params.product_category_id).then((product_category) => {
     const data = productCategoryView.prepareData(product_category)
     res.send({
       status: true,
