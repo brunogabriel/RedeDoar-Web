102fe20bf21a83f80deadf497fcf4c5d00ec7f40
Thiago Silva Ferreira <thiagosfsjc@gmail.com>
2017-03-18 22:25:23 -0300

    Ajuste na configuracao da url de callback do login do google

--- api/dist/config.js
+++ api/dist/config.js
@@ -20,7 +20,7 @@ config.facebook = {
 config.google = {
   appId: process.env.GOOGLE_APPID || '683942758553-1vv5fo0g0djsehickr1q29ereofu7tt5.apps.googleusercontent.com',
   appSecret: process.env.GOOGLE_APPSECRET || 'iomzfTKAsqIme2ZIN5H2B3nP',
-  redirectUri: process.env.GOOGLE_REDIRECTURI || 'http://rede-doar-api.dev.azk.io/users/google/callback',
+  redirectUri: process.env.GOOGLE_REDIRECTURI || config.rootUrl + 'users/google/callback',
   scopes: ['https://www.googleapis.com/auth/plus.me', 'https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile']
 };
 
